3040346999eadb8c3d98aa1d0a7436229d790acb
Cong Ye <eyecong@msn.com>
2019-04-11 16:13:13 -0600

    For test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var cordova: any;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,5 +12,9 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+
+  openBrowser(url) {
+    cordova.plugins.browsertab.themeable.openUrl(url, {toolbarColor:"#000000", closebuttoncaption : 'X', closeButtonEvent: 'closePressed'})
+    }
 
 }
